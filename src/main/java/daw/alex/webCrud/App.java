fe6edce5a4a7b4393a8a31619d4fdaf4462f81e1
Alex Valencia <alex32.com@gmail.com>
2015-05-21 09:13:53 +0200

    Actualizacion del pom.xml

---
+++ src/main/java/daw/alex/webCrud/App.java
@@ -0,0 +1,12 @@
+package daw.alex.webCrud;
+
+/**
+ * Proyecto Para Entornos de Desarollo y Bases de Datos
+ * CRUD de Videojuegos.
+ *
+ */
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
